b18065eade3a889f3f3f7d5b11f4f46f734d191a
aboujohn <abou.john@gmail.com>
2025-05-28 08:40:07 -0600

    docs: add GitHub templates and Git workflow conventions

--- commit-changes.bat
+++ commit-changes.bat
@@ -2,4 +2,4 @@
 echo Committing changes...
 "C:\Program Files\Git\cmd\git.exe" add .
 "C:\Program Files\Git\cmd\git.exe" commit -m "docs: add GitHub templates and Git workflow conventions"
-"C:\Program Files\Git\cmd\git.exe" push origin develop 
+"C:\Program Files\Git\cmd\git.exe" push origin main 
